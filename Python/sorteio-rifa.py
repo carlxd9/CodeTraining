9be6097cfcb8811443ed02f988770cb3a05cd8ae
carlxd9 <sol.rac.g9@gmail.com>
2021-11-01 17:49:33 -0300

    Sorting (dramatically) with Python

--- Python/sorteio-rifa.py
+++ Python/sorteio-rifa.py
@@ -1,5 +1,4 @@
-import random
-import time
+import random, time
 
 print("CARTELA DA SORTE - - - - - \n")
 rifa = ['Regina','Débora','Tereza','Áurea','Elvira','Isabel','Margarida','Isaura','Selma','Dulce','Zoraide','Raquel','Adriana','Simone','Lourdes','Dolores','Roberta','Dirce','Norma','Daniela','Rosália','Joelma','Glória','Fátima','Zuleika','Jurema','Mônica','Helena','Eugênia','Adelaide','Denise','Jandira','Rosana','Valéria','Celeste','Olinda','Emília','Jaqueline','Adélia','Patrícia','Miriam','Clarice','Aurora','Lenice','Solange','Ivone','Márcia','Dora','Graziela','Maria']
