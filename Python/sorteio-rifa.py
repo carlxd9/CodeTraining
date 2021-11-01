e59c9fbba6c04646ca6d9f882b4b1c742824ac44
carlxd9 <sol.rac.g9@gmail.com>
2021-11-01 17:43:53 -0300

    Sorting (dramatically) with Python

---
+++ Python/sorteio-rifa.py
@@ -0,0 +1,15 @@
+import random
+import time
+
+print("CARTELA DA SORTE - - - - - \n")
+rifa = ['Regina','Débora','Tereza','Áurea','Elvira','Isabel','Margarida','Isaura','Selma','Dulce','Zoraide','Raquel','Adriana','Simone','Lourdes','Dolores','Roberta','Dirce','Norma','Daniela','Rosália','Joelma','Glória','Fátima','Zuleika','Jurema','Mônica','Helena','Eugênia','Adelaide','Denise','Jandira','Rosana','Valéria','Celeste','Olinda','Emília','Jaqueline','Adélia','Patrícia','Miriam','Clarice','Aurora','Lenice','Solange','Ivone','Márcia','Dora','Graziela','Maria']
+
+resultado = ['O','nome','escolhido','foi:']
+for i in resultado:
+    print(i, flush=True, end=' ')  # flush permite o end dentro do loop
+    # end=" " pra não pular linha depois de executar 
+    time.sleep(0.5)
+
+print("\n")
+print(random.choice(rifa))
+print("\n")
